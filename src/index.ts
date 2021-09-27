e9ef417f9c79c9b85ec003545ea68179971c1088
tyufjvbn2 <tyufjvbn22@gmail.com>
2021-09-27 11:15:57 +0900

    checking other date

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,68 @@
 import express from "express";
 import axios from "axios";
+import FormData from "form-data";
 import cheerio from "cheerio";
 
 const app = express();
 
-// const getHtml = async () => {
-// 	try {
-// 		return await axios.get(
-// 			"https://base.uplus.co.kr/match/20210922/20210922KTHT0/smsRelay"
-// 		);
-// 	} catch (err) {
-// 		console.error(err);
-// 	}
-// };
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const postBody = {
+	ctl00$ctl00$ctl00$cphContents$cphContents$cphContents$scriptmanager1: `ctl00$ctl00$ctl00$cphContents$cphContents$cphContents$udpRecord |
+		ctl00$ctl00$ctl00$cphContents$cphContents$cphContents$btnNextDate`,
+	ctl00$ctl00$ctl00$cphContents$cphContents$cphContents$hfSearchDate:
+		"20210927",
+	__EVENTTARGET:
+		"ctl00$ctl00$ctl00$cphContents$cphContents$cphContents$btnNextDate",
+	__VIEWSTATE:
+		"/wEPDwUJNTI3NTA0NTg1D2QWAmYPZBYCZg9kFgJmD2QWAgIBD2QWAmYPZBYCAgEPZBYCAgMPZBYCAgEPZBYCZg9kFgYCAw8PFgIeBFRleHQFDzIwMjEuMDkuMjco7JuUKWRkAgcPFgIeC18hSXRlbUNvdW50ZmQCCQ8PFgIeB1Zpc2libGVnZGRktlcHQfoidliNudWx6f7RA9BGtfALQpDkzGgW6B/3tps=",
+	__VIEWSTATEGENERATOR: "169B9AF4",
+	__EVENTVALIDATION:
+		"/wEdAAUJ0BN6NlWOaqm5pRfE62dLlEwcu7CwxNIMjgoCNV+DoOyPE1qDl/G2n2/9ckf4rHIe0OpQfhxR5FmU2jfEHnpq0+3SNFh3Hpdaa49BEvunoM0VrI6eZoe6S7NNzcUAejUFizuChGD8IFts1PIkmXAi",
+	__ASYNCPOST: "true",
+};
+let form = new FormData();
+form.append(
+	"ctl00$ctl00$ctl00$cphContents$cphContents$cphContents$scriptmanager1",
+	`ctl00$ctl00$ctl00$cphContents$cphContents$cphContents$udpRecord |
+		ctl00$ctl00$ctl00$cphContents$cphContents$cphContents$btnNextDate`
+);
+form.append(
+	"ctl00$ctl00$ctl00$cphContents$cphContents$cphContents$hfSearchDate",
+	"20210927"
+);
+form.append("__EVENTTARGET", postBody.__EVENTTARGET);
+form.append(
+	"__VIEWSTATE",
+	"/wEPDwUJNTI3NTA0NTg1D2QWAmYPZBYCZg9kFgJmD2QWAgIBD2QWAmYPZBYCAgEPZBYCAgMPZBYCAgEPZBYCZg9kFgYCAw8PFgIeBFRleHQFDzIwMjEuMDkuMjco7JuUKWRkAgcPFgIeC18hSXRlbUNvdW50ZmQCCQ8PFgIeB1Zpc2libGVnZGRktlcHQfoidliNudWx6f7RA9BGtfALQpDkzGgW6B/3tps="
+);
+// form.append("__VIEWSTATEGENERATOR", postBody.__VIEWSTATEGENERATOR);
+form.append("__EVENTVALIDATION", postBody.__EVENTVALIDATION);
+form.append("__ASYNCPOST", postBody.__ASYNCPOST);
+
+const getHtml = async () => {
+	try {
+		return await axios.post(
+			"https://www.koreabaseball.com/Schedule/ScoreBoard.aspx",
+			form,
+			{
+				headers: {
+					"User-Agent":
+						"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
+					// "Content-Type":
+					// 	"application/x-www-form-urlencoded; charset=UTF-8",
+				},
+			}
+		);
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+Promise.all([getHtml()]).then((val) => {
+	console.log("html?", val);
+});
 
 // getHtml()
 // 	.then((html: any) => {
