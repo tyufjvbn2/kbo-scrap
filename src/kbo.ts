5f7a006b575da41024f90cd1a95a18af5488cbd5
tyufjvbn2 <tyufjvbn22@gmail.com>
2021-09-29 11:50:32 +0900

    fix: change updater keep finding time

--- src/kbo.ts
+++ src/kbo.ts
@@ -29,36 +29,46 @@ const start = () => {
 };
 
 //하루 한번만(날짜 바뀔때)
-// schedule.scheduleJob("0 0 0 * * *", () => {
-start().then((data: any) => {
-	console.log("your shape?", data);
-	create(data.totalGame);
-	console.log("start time?", data.startTime);
-	startTime = data.startTime.split(":");
+schedule.scheduleJob("0 0 0 * * *", () => {
+	start().then((data: any) => {
+		console.log("your shape?", data);
+		create(data.totalGame);
+		console.log("start time?", data.startTime);
+		startTime = data.startTime.split(":");
 
-	console.log("checker?", data.gameChecker);
-});
-// });
-
-//시작시간을 catch하지 못한경우
-if (startTime.length === 0) {
-	console.error("We failed to catch time of game start");
-} else {
-	//경기 시작 30분 전부터 끝나고 30분 후까지 계속
-	schedule.scheduleJob(`0 ${startTime[1]} ${startTime[0]} * * *`, () => {
-		start().then((data: any) => {
-			//종료할건지 계속 체크해야 함
-			const repeat = setInterval(() => {
-				if (data.gameChecker !== data.totalGame.length) {
-					update(data.totalGame);
-				} else {
-					console.log("All game end");
-					clearInterval(repeat);
-				}
-			}, 8000);
-		});
+		console.log("checker?", data.gameChecker);
 	});
-}
+});
+
+const updater = () => {
+	//시작시간을 catch하지 못한경우
+	if (startTime.length === 0) {
+		console.error("We failed to catch time of game start");
+		start().then((data: any) => {
+			console.log("Trying to catch time...");
+			startTime = data.startTime.split(":");
+			return updater();
+		});
+	} else {
+		console.log("We catch game start time. It will be start on time");
+		//지정된 경기 시작시간에 start, 모든 경기 끝나는 순간 end
+		schedule.scheduleJob(`0 ${startTime[1]} ${startTime[0]} * * *`, () => {
+			start().then((data: any) => {
+				//종료할건지 계속 체크해야 함
+				const repeat = setInterval(() => {
+					if (data.gameChecker !== data.totalGame.length) {
+						update(data.totalGame);
+					} else {
+						console.log("All game end");
+						clearInterval(repeat);
+					}
+				}, 8000);
+			});
+		});
+	}
+};
+
+updater();
 
 const router = express.Router();
 
