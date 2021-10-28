77808c96a0444ef7507de82f6ae95bf34300f107
tyufjvbn2 <tyufjvbn22@gmail.com>
2021-10-28 13:10:23 +0900

    test version for adv

--- src/test.ts
+++ src/test.ts
@@ -4,27 +4,50 @@ import cheerio from "cheerio";
 const getHtml = async () => {
 	try {
 		return await axios.get(
-			"https://sports.news.naver.com/kbaseball/record/index?category=kbo"
+			"https://www.koreabaseball.com/Schedule/ScoreBoard.aspx"
 		);
 	} catch (err) {
 		console.error(err);
 	}
 };
 
-getHtml()
-	.then((html: any) => {
-		let arr: any[] = [];
-		const $ = cheerio.load(html.data);
-		const bodyList = $("tbody#regularTeamRecordList_table").children("tr");
-		console.log("anybody there?", bodyList);
+getHtml().then((html: any) => {
+	const dictionary = ["KT", "한화", "SSG"];
+	let arr: any[] = [];
+	let startTime: any[] = [];
+	let inningState: any[] = [];
+	let dhCheck: any[] = [];
+	const $ = cheerio.load(html.data);
+	// const htmlDoc = $("body");
+	// console.log("html body", $.html()); //전체 html 확인
 
-		bodyList.each(function (i, ele) {
-			// console.log("what is I", i);
-			arr[i] = {
-				team: $(this).find("td.tm span").text(),
-			};
-		});
+	const playBoardList = $(
+		"div#cphContents_cphContents_cphContents_udpRecord"
+	).children("div.smsScore");
 
-		return arr;
-	})
-	.then((res) => console.log(res));
+	// console.log("anybody there?", bodyList);
+
+	playBoardList.each((i, ele) => {
+		// console.log("what is I", i);
+		// console.log($(ele).html());
+
+		if (
+			dictionary.indexOf(
+				$(ele).find("p.rightTeam strong.teamT").text()
+			) !== -1
+		) {
+			arr.push($(ele).find("p.rightTeam strong.teamT").text());
+			startTime.push($(ele).find("p.place").text());
+			inningState.push($(ele).find("strong.flag span").text());
+			dhCheck.push($(ele).find("p.dh").text());
+		}
+	});
+
+	console.log("team get", arr);
+	console.log("start time get", startTime);
+	console.log("inning state get", inningState);
+	console.log("doubl header get", dhCheck);
+
+	// return arr;
+});
+// .then((res) => console.log(res));
